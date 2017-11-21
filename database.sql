999ef1019eca3309bc94daa8f0dd4bf9f900b96f
Stephanie Ratanas <stephanieratanas@gmail.com>
2017-11-21 10:13:36 -0600

    add database queries

---
+++ database.sql
@@ -0,0 +1,10 @@
+CREATE TABLE shoes (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(80),
+	cost INTEGER
+	);
+    	
+INSERT INTO shoes (name, cost)
+VALUES ('cole haan', 200), 
+('Frye veronica short', 380),
+('Space Boots', 10);
